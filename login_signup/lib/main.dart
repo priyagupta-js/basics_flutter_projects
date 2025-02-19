0ad95242361e0678d2f992faede22f0d6a582d89
Priya Gupta <priyagupta052k@gmail.com>
2025-02-19 23:14:43 +0530

    inital files

---
+++ login_signup/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+// import 'package:login_signup/Signup.dart';
+import 'package:login_signup/Login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Login(),
+  ));
+}
