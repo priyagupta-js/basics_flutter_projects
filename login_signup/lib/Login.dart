316f601a813c4175d9c3aefc16dfec2469bb9724
Priya Gupta <priyagupta052k@gmail.com>
2025-02-20 19:34:14 +0530

    Update Login Page

--- login_signup/lib/Login.dart
+++ login_signup/lib/Login.dart
@@ -1,12 +1,55 @@
+// import 'package:flutter/material.dart';
+
+// class Login extends StatelessWidget {
+//   const Login({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: Column(
+//       children: [
+//         Container(
+//           color: Colors.amberAccent,
+//           padding: EdgeInsets.all(10),
+//           margin: EdgeInsets.all(10),
+//           child: Text(
+//             'Login',
+//             style: TextStyle(
+//               color: Colors.white,
+//               fontSize: 25,
+//               fontWeight: FontWeight.bold,
+//               letterSpacing: 2,
+//             ),
+//           ),
+//         ),
+//         Container(
+//           child: Text('username'),
+//         ),
+//         Container(
+//           child: Text('Password'),
+//         ),
+//       ],
+//     ));
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
 
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('login page'),
+    return Column(
+      children: [
+        Container(
+          child: Text('Login Page'),
+        )
+      ],
     );
   }
 }
--- login_signup/lib/main.dart
+++ login_signup/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:login_signup/Signup.dart';
+// import 'package:login_signup/Signup.dart';
 import 'package:login_signup/Login.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Signup(),
+    home: Scaffold(
+      body: Login(),
+    ),
   ));
 }
