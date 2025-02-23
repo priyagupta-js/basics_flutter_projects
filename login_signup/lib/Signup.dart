88521e73fb4e9aabee49a575342aea18dba20343
Priya Gupta <priyagupta052k@gmail.com>
2025-02-23 23:07:54 +0530

    SignUp page

--- login_signup/lib/Signup.dart
+++ login_signup/lib/Signup.dart
@@ -6,27 +6,60 @@ class Signup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
+      // appBar: AppBar(title: Text('Login')),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(30),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text('Sign Up',
+                style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            // name
             TextField(
               // controller: _usernameController,
-              decoration: InputDecoration(labelText: 'Username'),
+              decoration: InputDecoration(labelText: 'Name'),
             ),
-            SizedBox(height: 16),
+            SizedBox(height: 20),
+            // email
+            TextField(
+              // controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            SizedBox(height: 20),
+            // phone number
+            TextField(
+              // controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Phone Number'),
+            ),
+            SizedBox(height: 20),
             TextField(
               // controller: _passwordController,
               decoration: InputDecoration(labelText: 'Password'),
               obscureText: true,
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Login'),
+            TextField(
+              // controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Confirm Password'),
+              obscureText: true,
             ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Register'),
+              ),
+            )
           ],
         ),
       ),
