1c299f9a4cd92eef1d2837ec8212cfd70e0d7422
Priya Gupta <priyagupta052k@gmail.com>
2025-04-22 23:31:24 +0530

    login auth

--- login_signup/lib/main.dart
+++ login_signup/lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:login_signup/Signup.dart';
 import 'package:login_signup/Login.dart';
+import 'package:login_signup/firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MaterialApp(
     home: Login(),
   ));
